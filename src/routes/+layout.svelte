ddba60abaa593f9ce8f187515773630c03b10f68
vtempest <you@example.com>
2024-12-26 00:28:52 -0800

    footer is back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,12 @@
   import '../app.css';
 
   import Header from "$components/header.svelte"
+  import Footer from "$components/footer.svelte"
+  
 </script>
 
 <div class="container">
   <Header />
   <slot />
+  <Footer />
 </div>
